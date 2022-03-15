b7bc733a93f16c1edc9f2a214bb275ce15b28e38
Olisemedua Onwuatogwu <oonwuatogwu@csumb.edu>
2022-03-15 16:35:43 -0700

    Just the basic layout for the homepage

--- src/main/java/com/example/springbootstarterthymeleaf/Api.java
+++ src/main/java/com/example/springbootstarterthymeleaf/Api.java
@@ -1,8 +1,7 @@
 package com.example.springbootstarterthymeleaf;
 
 
-import com.example.springbootstarterthymeleaf.database.User;
-import com.example.springbootstarterthymeleaf.database.UserRepository;
+import com.example.springbootstarterthymeleaf.database.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +14,12 @@ public class Api {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private WishlistRepository wishlistRepository;
+    @Autowired
+    private ItemRepository itemRepository;
 
+    //User API endpoints
     @RequestMapping("/getAllUsers")
     public @ResponseBody Iterable<User> getAllUsers() {
         return userRepository.findAll();
@@ -29,5 +33,22 @@ public class Api {
     @RequestMapping("/getUserByUserId")
     public @ResponseBody User getUserByUserId(@RequestParam(defaultValue = "0") Integer userId){
         return userRepository.findByUserId(userId);
+    }
+
+    //Wishlist API endpoints
+    @RequestMapping("/getWishlistByUserId")
+    public @ResponseBody WishList getWishlistByUserId(@RequestParam(defaultValue = "0") Integer userId){
+        return wishlistRepository.findWishListByUserId(userId);
+    }
+
+    @RequestMapping("/getWishlistByWishlistId")
+    public @ResponseBody WishList getWishlistByWishlistId(@RequestParam(defaultValue = "0") Integer wishlistId){
+        return wishlistRepository.findWishListByWishListId(wishlistId);
+    }
+
+    //Item API endpoints
+    @RequestMapping("/getItemByItemId")
+    public @ResponseBody Item getItemByItemId(@RequestParam(defaultValue = "0") Integer itemId){
+        return itemRepository.findItemByItemId(itemId);
     }
 }
---
+++ src/test/java/com/example/springbootstarterthymeleaf/AmazonImageURLScraperTest.java
@@ -0,0 +1,33 @@
+package com.example.springbootstarterthymeleaf;
+
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.io.IOException;
+import java.util.Objects;
+
+import static com.example.springbootstarterthymeleaf.database.AmazonImageURLScraper.getAmazonImageURL;
+
+public class AmazonImageURLScraperTest {
+
+    @Test
+    void TestSimpleItem() throws IOException {
+        String AmazonURL = "https://www.amazon.com/Pepe-Frog-official-anatomically-correct/dp/B01MCT549R/";
+        String expectedImageURL = "https://images-na.ssl-images-amazon.com/images/I/61dnV42UYaL.__AC_SX300_SY300_QL70_ML2_.jpg";
+
+        String ImageURL = getAmazonImageURL(AmazonURL);
+
+        assert (expectedImageURL.equals(ImageURL));
+    }
+
+    @Test
+    void TestBookItem() throws IOException {
+        String AmazonURL = "https://www.amazon.com/Sonichu-0-C-W/dp/1699276854/";
+        String expectedImageURL = "https://images-na.ssl-images-amazon.com/images/I/51F9RP7npiL._SX218_BO1,204,203,200_QL40_ML2_.jpg";
+
+        String ImageURL = getAmazonImageURL(AmazonURL);
+
+        assert (expectedImageURL.equals(ImageURL));
+    }
+}
+
